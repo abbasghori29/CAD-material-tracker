262f7f43e352f52c6b039a17e4f5b0f8799d1e8c
Abbas Ghori <abbasghori0229@gmail.com>
2025-12-03 21:56:22 +0500

    Fix uvicorn settings - use defaults

--- app.py
+++ app.py
@@ -680,8 +680,5 @@ if __name__ == "__main__":
     uvicorn.run(
         app, 
         host="0.0.0.0", 
-        port=8000,
-        timeout_keep_alive=0,   # No timeout
-        ws_ping_interval=None,  # Disable uvicorn ping (we have our own heartbeat)
-        ws_ping_timeout=None    # No timeout
+        port=8000
     )
